80189e5b87a30b4839dc611bc7e6c2c49a114606
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-03 15:30:00 -0500

    Paso 24) Agregando clase static getExplorersUsernamesByMission, eh ingresando parametros, haciendo correr las pruebas unitarias y funcionando OK

--- test/controllers/explorerController.test.js
+++ test/controllers/explorerController.test.js
@@ -5,4 +5,9 @@ describe('Metodos para obtener lista de explorers', () => {
         const explorersByMission = ExplorerController.getExplorersByMission("node")
         expect(explorersByMission[3].name).toBe("Woopa4");
     })
+    test('2. Retornando Username de explorers en la mision node', () => {
+        const explorersmission = ExplorerController.getExplorersByMission("node")
+        const usernames = explorersmission.map((explorer) => explorer.githubUsername)
+        expect(usernames[3]).toMatch("ajolonauta4");
+    })
 })
