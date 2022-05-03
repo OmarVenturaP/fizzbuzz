489ead5ced9ba3557353fac5487f56900255c6d1
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-03 02:53:11 -0500

    Paso 16) Haciendo fallar automatizacion de prueba

--- test/services/fizzbuzzService.test.js
+++ test/services/fizzbuzzService.test.js
@@ -14,7 +14,7 @@ describe('Validacion de explorers con diferentes scores', () => {
     test("3. Explorer 5, score 5", () => {
         const explorer5 = {name: "Explorer5", score: 5}
         const validationExplorer = FizzbuzzService.applyValidationInExplorer(explorer5)
-        expect(validationExplorer.trick).toBe("BUZZ");
+        expect(validationExplorer.trick).toBe("FBUZZ");
     })
     test("4. Explorer 15, score 15", () => {
         const explorer15 = {name: "Explorer15", score: 15}
