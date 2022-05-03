828df19d52c561885c65df89e9f627815b380615
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-03 00:41:11 -0500

    Paso 4) Agregando classs Reader, creando archivo de prueba y probando funcionamiento OK

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const explorers = JSON.parse(rawdata);
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+// console.log(explorersInNode.length)
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
 const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+// console.log(usernamesInNode)
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
---
+++ test/utils/reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require('../../lib/utils/reader');
+
+describe('Test reader app', ()=> {
+    test('should first', () => {
+        const readers = Reader.readJsonFile('explorers.json');
+        expect(readers).toBeDefined();
+     })
+})
