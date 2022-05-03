3eef4808735e824219ca3df2830b05efe5890379
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-03 16:11:32 -0500

    Paso 31) Corriendo linter para corregir inconsistencias de escritura y mejorar la legibilidad del código -fix

--- test/controllers/explorerController.test.js
+++ test/controllers/explorerController.test.js
@@ -1,18 +1,18 @@
-const ExplorerController = require('../../lib/controllers/explorerController');
+const ExplorerController = require("../../lib/controllers/explorerController");
 
-describe('Metodos para obtener lista de explorers', () => {
-    test('1. Retornando explorer por mision', () => {
-        const explorersByMission = ExplorerController.getExplorersByMission("node")
+describe("Metodos para obtener lista de explorers", () => {
+    test("1. Retornando explorer por mision", () => {
+        const explorersByMission = ExplorerController.getExplorersByMission("node");
         expect(explorersByMission[3].name).toBe("Woopa4");
-    })
-    test('2. Retornando Username de explorers en la mision node', () => {
-        const explorersMission = ExplorerController.getExplorersByMission("node")
-        const usernames = explorersMission.map((explorer) => explorer.githubUsername)
+    });
+    test("2. Retornando Username de explorers en la mision node", () => {
+        const explorersMission = ExplorerController.getExplorersByMission("node");
+        const usernames = explorersMission.map((explorer) => explorer.githubUsername);
         expect(usernames[3]).toMatch("ajolonauta4");
-    })
-    test('3. Retornando la cantidad de explorers en la mision de node', () =>{
-        const explorersMission = ExplorerController.getExplorersByMission("node")
-        const explorersAmount = explorersMission.length
+    });
+    test("3. Retornando la cantidad de explorers en la mision de node", () =>{
+        const explorersMission = ExplorerController.getExplorersByMission("node");
+        const explorersAmount = explorersMission.length;
         expect(explorersAmount).toBe(10);
-    })
-})
+    });
+});
