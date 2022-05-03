d7666c0c98f96d92559d9f94d141488e065c90c4
Omar Ventura <99059639+OmarVenturaP@users.noreply.github.com>
2022-05-03 02:15:24 -0500

    Paso 10) Creando prueba unitaria de validacion de explorer 3 para fizzbuzzService} y funcionando OK

--- test/services/fizzbuzzService.test.js
+++ test/services/fizzbuzzService.test.js
@@ -6,4 +6,9 @@ describe('Validacion de explorers con diferentes scores', () => {
         const validationExplorer = FizzbuzzService.applyValidationInExplorer(explorer1)
         expect(validationExplorer.trick).toBe(1);
     })
+    test("2. Explorer 3, score 3", () => {
+        const explorer3 = {name: "Explorer3", score: 3}
+        const validationExplorer = FizzbuzzService.applyValidationInExplorer(explorer3)
+        expect(validationExplorer.trick).toBe("FIZZ");
+    })
 })
